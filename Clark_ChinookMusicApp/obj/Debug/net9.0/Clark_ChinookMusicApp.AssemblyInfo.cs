b4584e7ea0f4e78f16f5074a18588b818173ba7c
brjclark09 <brjclark09@gmail.com>
2025-06-08 10:36:40 -0500

    asdf

--- Clark_ChinookMusicApp/obj/Debug/net9.0/Clark_ChinookMusicApp.AssemblyInfo.cs
+++ Clark_ChinookMusicApp/obj/Debug/net9.0/Clark_ChinookMusicApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clark_ChinookMusicApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6006f08c03c02fc7c6cfda31799933e8ac27a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0976fbff8d004a81a21b392ba15a0abd49bfae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clark_ChinookMusicApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clark_ChinookMusicApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

473d16ad4dd230ac76c0327f5bf55d9399bb29f1
brjclark09 <79394793+brjclark09@users.noreply.github.com>
2025-06-11 11:41:40 -0500

    start

--- Clark_ChinookEnhancedQueries/Services/EnhancedQueryService.cs
+++ Clark_ChinookEnhancedQueries/Services/EnhancedQueryService.cs
@@ -1,9 +1,25 @@
 namespace Clark_ChinookEnhancedQueries.Services;
 
-public class EnhancedQueryService
+public class EnhancedQueryService : IEnhancedQuery
 {
+    private readonly ApplicationDbContext _context;
+
+    public EnhancedQueryService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public async Task<Dictionary<string, List<Customer>>> CustomersGroupedBySupportRepAsync()
     {
-        
+        var result = await _context.Customer
+            .GroupBy(customer => customer.SupportRepId)
+            .ToDictionaryAsync(
+                group => group.Key.HasValue 
+                    ? _context.Employee.Where(eemployee => employee.EmployeeId == group.Key.Value).Select(employee => employee.FirstName + " " + employee.LastName).FirstOrDefault() ?? "No Rep"
+                    : "No Rep",
+                g => group.ToList()
+            );
+        await _context.SaveChangesAsync();
+        return result;
     }
 }
